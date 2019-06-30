30e7ff6b2ba12ec1f5de512b97bc7e0e072df635
Lifajia <lifajia147@163.com>
2019-06-30 15:04:45 +0800

    GIT学习

--- src/main/java/com/lifajia/util/Test.java
+++ src/main/java/com/lifajia/util/Test.java
@@ -3,5 +3,6 @@ package com.lifajia.util;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello World!!!!!!!!!!!!!!!!!!!!!");
     }
 }
